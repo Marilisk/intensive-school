a92c68cb6863910a742c0eb2496417ed6e29a2ac
Marilisk <6868221@gmail.com>
2023-01-25 15:53:32 +0300

    content type changed

--- src/Components/Test/FinalPage.tsx
+++ src/Components/Test/FinalPage.tsx
@@ -37,7 +37,7 @@ export const FinalPage: FC<IFinalPage> = ({ scoreSum, questionsAmount, currentTe
             url: 'https://intensiv.ru/system/testresult.php',
             headers: {
                 /* 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', */
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Content-Type': 'application/json; charset=utf-8'
             },
             data: result
         })
